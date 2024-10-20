33db992fdce8e990243a2622574e38da9007b952
Fotocoop <carlosmito2411@gmail.com>
2024-10-19 20:38:36 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function togglePasswordVisibility() {
       passwordInput.type = "password";
     }
   }
+
+  const slides = document.querySelectorAll('.slide');
+  let currentSlide = 0;
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (n + slides.length)   
+% slides.length;
+    slides[currentSlide].classList.add('active');   
+
+  }
+
+  // Mostrar la siguiente imagen cada 3 segundos (ajusta el tiempo según necesites)
+  setInterval(function() {
+    showSlide(currentSlide + 1);
+  }, 3000);
